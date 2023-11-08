9fbe59b3afc63190d4c69fc9780065f76fe254b4
tssingtampa <ingenierostss@gmail.com>
2023-11-07 19:06:04 -0500

    Primer commit

---
+++ docker/imposter/agents/agents.js
@@ -0,0 +1,29 @@
+var agentId = context.request.queryParams.agents_list;
+
+switch (agentId) {
+  case undefined:
+    respond().withStatusCode(200).withFile('agents/agents.json');
+    break;
+  case '001':
+    respond().withStatusCode(200).withFile('agents/agent_active_groups.json');
+    break;
+  case '003':
+  case '010':
+    respond().withStatusCode(200).withFile('agents/agent_disconnected.json');
+    break;
+  case '004':
+  case '007':
+  case '008':
+  case '009':
+    respond().withStatusCode(200).withFile('agents/agent_never_connected.json');
+    break;
+  case '005':
+    respond().withStatusCode(200).withFile('agents/agent_macos.json');
+    break;
+  case '006':
+    respond().withStatusCode(200).withFile('agents/agent_pending.json');
+    break;
+  default:
+    respond().withStatusCode(200).withFile('agents/agent.json');
+    break;
+}
